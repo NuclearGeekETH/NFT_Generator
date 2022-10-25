35bb2ae34298e0896c452eda6d51e1978ddc8d24
Guy Mann <guydmann@gmail.com>
2022-10-25 19:46:43 -0400

    rewrote the rename script to use argparse and to stop you if you are about to rename your script directory

--- rename.py
+++ rename.py
@@ -2,18 +2,32 @@
 # sort all files in natural order then renumber the files starting at 4001
 
 import os
-import sys
+import argparse
 import re
 
-# get the current working directory
-cwd = os.getcwd()
 
-# get the list of files in the current working directory
-files = os.listdir(cwd)
+def rename_files(directory=None):
+    if not directory:
+        # get the current working directory
+        directory = os.getcwd()
+    if directory == os.path.dirname(__file__):
+        print("ERROR: You are about to rename all the files in the directory containing this scripts")
+        exit()
 
-# sort the files in natural order
-files.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split('(\d+)', x)])
+    # get the list of files in the current working directory
+    files = os.listdir(directory)
 
-# renumber the files starting at 4001
-for i in range(len(files)):
-    os.rename(files[i], str(i+4001)+'.json')
+    # sort the files in natural order
+    files.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split('(\d+)', x)])
+
+    # renumber the files starting at 4001
+    for i in range(len(files)):
+        print("renaming {name}".format(name=str(i+4001)+'.json'))
+        #os.rename(files[i], str(i+4001)+'.json')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('-d', '--directory', default=None, type=str, help='the directory containing the file you want to rename')
+    arguments = parser.parse_args()
+    rename_files(arguments.directory)
