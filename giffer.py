f99628bb21166c9fb6f18b5a231d3e57fa9fa6b0
Guy Mann <guydmann@gmail.com>
2022-10-25 21:08:59 -0400

    added a progress bar and some extra output

--- giffer.py
+++ giffer.py
@@ -1,27 +1,31 @@
 # make a gif from frames in a folder
 import argparse
-import imageio
+import imageio.v2 as imageio
 import os
+from tqdm import tqdm
 
 
 def make_gif(inpath: str, outpath: str, delay: float, final_delay: float, loop: bool):
     # get images
     images = []
-    for f in sorted(os.listdir(inpath)):
+    print("Combining images")
+    for f in tqdm(sorted(os.listdir(inpath))):
         if f.endswith('.png'):
             images.append(imageio.imread(inpath+'/'+f))
     # save gif
+    print("Saving gif, this may take a few minutes depending on the number of images you are combining")
     imageio.mimsave(outpath, images, duration=delay, loop=loop)
     # add final delay frame
     if final_delay:
         imageio.imwrite(outpath, imageio.core.util.Array([0,0,0,0]).astype('uint8'), format='GIF', duration=final_delay)
+    print("Gif creation complete")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('-i', '--inpath', type=str, default='build/images/',
                         help='directory containing the png images to combine into a gif')
-    parser.add_argument('-i', '--outpath', type=str, default='gif/output.gif',
+    parser.add_argument('-o', '--outpath', type=str, default='gif/output.gif',
                         help='the output filename and directory for the final animated gif')
     parser.add_argument('-d', '--delay', type=float, default=0.2,
                         help='the frame delay between images')
