2b98db577109dd246f30815f048f0d39c4127752
Guy Mann <guydmann@gmail.com>
2022-10-25 20:25:51 -0400

    changed the removeJSONendings script to be more modular

--- removeJSONending.py
+++ removeJSONending.py
@@ -2,7 +2,25 @@
 # remove .json from files in directory
 
 import os
+import argparse
 
-for filename in os.listdir("."):
-    if filename.endswith(".json"):
-        os.rename(filename, filename[:-5])
+
+def remove_json_ending(directory: str = None):
+    """
+    Removes the json extension from files in a directory
+    :param directory: the directory containing the files to rename
+    :return: nothing
+    """
+    if not directory:
+        directory = os.getcwd()
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            os.rename(filename, filename[:-5])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('-d', '--directory', default=None, type=str, help='the directory containing the files you want to rename')
+    arguments = parser.parse_args()
+    remove_json_ending(arguments.directory)
