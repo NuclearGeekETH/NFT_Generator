bfbd34f6187c26d8ad590c9beb4ec02844067632
Guy Mann <guydmann@gmail.com>
2022-10-25 20:08:12 -0400

    improving the rename script

--- rename.py
+++ rename.py
@@ -6,13 +6,26 @@ import argparse
 import re
 
 
-def rename_files(directory=None):
+
+def rename_files(directory: str = None, starting_offset: int = None):
+    """
+
+    :param directory: the directory containing the files to rename
+    :param starting_offset: the starting numeric value for the offset
+    :return: nothing
+    :raises ValueError if the starting_offset is not present or the
+    :raises Excpetion if the starting_offset is not present or the
+    """
+    if not starting_offset:
+        raise ValueError("The starting_offset is not set.")
+
     if not directory:
         # get the current working directory
         directory = os.getcwd()
-    if directory == os.path.dirname(__file__):
-        print("ERROR: You are about to rename all the files in the directory containing this scripts")
-        exit()
+
+    current_script_path = os.path.dirname(os.path.realpath(__file__))
+    if directory == current_script_path:
+        raise Exception("You are about to rename all the files in the directory containing this scripts, you probably want to run this in a different directory or specify the directory at the command line.")
 
     # get the list of files in the current working directory
     files = os.listdir(directory)
@@ -21,13 +34,18 @@ def rename_files(directory=None):
     files.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split('(\d+)', x)])
 
     # renumber the files starting at 4001
-    for i in range(len(files)):
-        print("renaming {name}".format(name=str(i+4001)+'.json'))
+    count = 0
+    for file in files:
+        print("renaming {orig} to {name}".format(orig=file, name=str(count+starting_offset)+'.json'))
         #os.rename(files[i], str(i+4001)+'.json')
+        count += 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument('-d', '--directory', default=None, type=str, help='the directory containing the file you want to rename')
+    parser.add_argument('-d', '--directory', default=None, type=str, help='the directory containing the files you want to rename')
+    parser.add_argument('-s', '--starting_offset', default=4001, type=int,
+                        help='the numerical offset to start the renaming at.')
     arguments = parser.parse_args()
-    rename_files(arguments.directory)
+    rename_files(arguments.directory, arguments.starting_offset)
+    
